26bdf86df8159bab429391855f9b208e231288c6
jdvfx <ju.dep@zoho.com>
2025-05-31 19:44:55 -0700

    renamed type to nodetype, update node list

--- hide_unwanted_nodes.py
+++ hide_unwanted_nodes.py
@@ -1,37 +1,32 @@
+# python startup script
 # script executed on startup
 # https://www.sidefx.com/docs/houdini/hom/locations.html
 # to be saved in : ~/houdiniXX.X/pythonX.Xlibs/
-# eg: ~/houdini19.0/python3.7libs/
+# eg: ~/houdini19.0/python3.7libs/uiready.py
 
 import hou
 
-# hide some nodes not in use.
-
+# hide some nodes I don't use.
 hidelist =[
-'Groom','groom','heightfield',
-'muscle','fem','unreal','game','unix',
-'crowd','agent','tissue','feather','labs',
-'gltf','usd','guide'
-'motion','kinefx'
+'wrinkle','onnx','gltf','ml','fem','unreal','unix',
+'muscle','agent','crowd','kinefx','motion','groom','fem','feather',
+'tissue','ris','apex','guide'
 ]
 
 keeplist = [
-'pyrobakevolume'
 ]
 
-debug = False
-
-if debug:
-    hidelist=['kinefx']
+hidden_nodes=[]
 
 cats = hou.nodeTypeCategories()
 for cat in cats:
     for type in cats[cat].nodeTypes():
         for node_to_hide in hidelist:
             if node_to_hide in type and type not in keeplist:
-                if debug:
-                    print(cat , type)
-                else:
-                    cats[cat].nodeTypes()[type].setHidden(1)
+                hidden_nodes.append(f"{cat} {type}")
+                cats[cat].nodeTypes()[type].setHidden(1)
 
+with open("/tmp/houdini_hidden_nodes.txt","w") as file:
+    for node in hidden_nodes:
+        file.write(f"{node}\n")
 
